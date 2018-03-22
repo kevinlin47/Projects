a3df8b3d96a928a6cd50ab7d388e88d6698074c9
Kevin Lin <kevinlin47@hotmail.com>
2018-03-22 18:05:40 -0400

    Add files via upload

---
+++ Graph/src/graph/Driver.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+
+/**
+ *
+ * @author Kevin Lin
+ */
+public class Driver {
+    
+        public static void main(String args[])
+        {   
+            //Test graph, passing in number of verticies 
+            Graph graph=new Graph(5);
+            graph.addEdge(0,1);
+            graph.addEdge(0,3);
+            
+            graph.addEdge(1,3);
+            graph.addEdge(1,4);
+            graph.addEdge(1,2);
+            
+            graph.addEdge(2,3);
+            
+            graph.printGraph();
+            
+            /*
+            //Test graph using default constructor
+            Graph graph2=new Graph();
+            graph2.addEdge(1,3);
+            graph2.addEdge(1,19);
+            
+            graph2.addEdge(3,5);
+            graph2.addEdge(3,4);
+            
+            graph2.addEdge(0,3);
+            
+            graph2.printGraph();
+            */
+            
+            //Test depth first search on example graph
+            graph.dfs(0);
+        }
+        
+}
