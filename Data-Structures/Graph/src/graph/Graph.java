adc8cb0456820c86bd45e2d08b413fbf2b84ac5c
Kevin Lin <kevinlin47@hotmail.com>
2018-03-22 16:52:40 -0400

    Add files via upload

--- Data-Structures/Graph/src/graph/Graph.java
+++ Data-Structures/Graph/src/graph/Graph.java
@@ -5,17 +5,46 @@
  */
 package graph;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  *
  * @author Kevin Lin
  */
 public class Graph {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    //variable to hold number of vertices
+    int v;
+    
+    //ArrayList of LinkedLists
+    ArrayList<LinkedList<Integer>> adjListArray;
+    
+    //Construct that takes the number of vertices
+    //as a parameter
+    Graph(int v)
+    {
+        this.v=v;
+        adjListArray=new ArrayList();
+        
+        //Create a list for each vertex
+        for (int i=0;i<v;++i)
+        {
+            adjListArray.add(new LinkedList());
+        }
+    }
+    
+    //Default Constructor
+    
+    public void addEdge(int src, int dest)
+    {   
+        //Create and edge between the source node
+        //and the destination node
+        adjListArray.get(src).addFirst(dest);
+        
+        //Undirected grapg, so we need to add an 
+        //edge from destination to source as well
+        adjListArray.get(dest).addFirst(src);
     }
     
 }
