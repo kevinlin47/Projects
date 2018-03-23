52ec9bb631079e57caecf95e51d2b6aaee3812e2
Kevin Lin <kevinlin47@hotmail.com>
2018-03-22 22:32:18 -0400

    Add files via upload

--- Data-Structures/Graph/src/graph/Driver.java
+++ Data-Structures/Graph/src/graph/Driver.java
@@ -27,6 +27,7 @@ public class Driver {
             
             graph.printGraph();
             
+            /*
             //Test graph using default constructor
             Graph graph2=new Graph();
             graph2.addEdge(1,3);
@@ -38,7 +39,32 @@ public class Driver {
             graph2.addEdge(0,3);
             
             graph2.printGraph();
+            */
             
+            //Test depth first search on example graph
+            graph.dfs(0);
+            System.out.println();
+            
+            //Test breadth first search on the following test graph
+            Graph g=new Graph(6);
+            g.addEdge(0,1);
+            
+            g.addEdge(1,3);
+            g.addEdge(1,2);
+            
+            g.addEdge(2,5);
+            g.addEdge(2,4);
+            
+            g.addEdge(3,5);
+            
+            g.addEdge(4,5);
+            g.addEdge(4,6);
+            
+            g.addEdge(6,5);
+            
+            g.printGraph();
+            
+            g.bfs(0);
         }
         
 }
--- Data-Structures/Graph/src/graph/Graph.java
+++ Data-Structures/Graph/src/graph/Graph.java
@@ -5,6 +5,7 @@
  */
 package graph;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -73,6 +74,54 @@ public class Graph {
             System.out.print("head");
             adjListArray.get(i).forEach(e->System.out.print(" -> "+e));
             System.out.println("\n");
+        }
+    }
+    
+    //Method performs a Depth First Search Traversal on a connected graph
+    //Given the starting vertex 
+    public void dfs(int startVertex)
+    {   
+        //Create a boolean array for testing if a vertext has
+        //already been visited
+        boolean visited[]=new boolean[v];
+        dfs(startVertex,visited);
+    }
+    
+    //Helper function for dfs method
+    private void dfs(int vertex,boolean visited[])
+    {
+        visited[vertex]=true;
+        System.out.print(vertex+" ");
+        
+        adjListArray.get(vertex).forEach(e->{
+            if (!visited[e])
+            {
+                dfs(e,visited);
+            }
+        });
+        
+    }
+    
+    //Method performs a Breadth First Search Traversal on a connected graph
+    //Given the starting vertex
+    public void bfs(int startVertex)
+    {
+        boolean visited[]=new boolean[v];
+        visited[startVertex]=true;
+        ArrayDeque<Integer> queue=new ArrayDeque();
+        queue.addLast(startVertex);
+        
+        while(!queue.isEmpty())
+        {
+            int s=queue.removeFirst();
+            System.out.print(s+" ");
+            adjListArray.get(s).forEach(e->{
+                if (!visited[e])
+                {   
+                    visited[e]=true;
+                    queue.addLast(e);
+                }
+            });
         }
     }
 }
