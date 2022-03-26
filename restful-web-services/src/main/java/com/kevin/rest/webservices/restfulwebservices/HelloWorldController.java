7af52214729279cee60a64b2f112c7d10a6c1b5d
Kevin Lin <kevinlin47@hotmail.com>
2022-03-25 20:52:32 -0400

    hello world get request

--- restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,19 @@
 package com.kevin.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//Controller
+@RestController
 public class HelloWorldController {
+	
+	//GET
+	//URI - /hello-world
+	//method - "Hello World"
+	@RequestMapping(method=RequestMethod.GET, path="/hello-world") 
+	public String hellowWorld() {
+		return "Hello World";
+	}
 
 }
