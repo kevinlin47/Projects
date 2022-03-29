c6af502deb6bc04e26706d2e1521d41910795b61
Kevin Lin <kevinlin47@hotmail.com>
2022-03-28 20:17:40 -0400

    create user data access object class

--- restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDaoService {
 	private static List<User> users = new ArrayList();
+	private static int usersCount = 3;
 	
 	static {
 		users.add(new User(1, "Adam", new Date()));
@@ -18,5 +19,25 @@ public class UserDaoService {
 	
 	public List<User> findAll() {
 		return users;
+	}
+	
+	public User save(User user) {
+		if (user.getId() == null) {
+			user.setId(++usersCount);
+		}
+		
+		users.add(user);
+		
+		return user;
+	}
+	
+	public User findOne(int id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		
+		return null;
 	}
 }
