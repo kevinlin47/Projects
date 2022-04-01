61c33748ab333297bfcc718a74661be28a8ea119
Kevin Lin <kevinlin47@hotmail.com>
2022-03-31 21:36:39 -0400

    hateoas

--- restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/UserResource.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,14 +31,19 @@ public class UserResource {
 	}
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		
 		if (user == null) {
 			throw new UserNotFoundException("id - " + id);
 		}
 		
-		return user;
+		EntityModel<User> model = EntityModel.of(user);
+		
+		WebMvcLinkBuilder linkToUsers = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+		model.add(linkToUsers.withRel("all-users"));
+		
+		return model;
 	}
 	
 	@PostMapping("/users")
