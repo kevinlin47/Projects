7c19c20a8b42a355d43f7fc6e9b63d0d365837b7
Kevin Lin <kevinlin47@hotmail.com>
2022-04-14 20:36:54 -0400

    h2 in-memory db

--- restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/User.java
@@ -2,11 +2,17 @@ package com.kevin.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 characters")
