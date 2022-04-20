6ebb431d31b7a47c0cbaeff10854fa88aa5311c3
Kevin Lin <kevinlin47@hotmail.com>
2022-04-20 19:54:21 -0400

    create post entity

--- restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,12 @@
 package com.kevin.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -21,6 +23,17 @@ public class User {
 	@Past
 	private Date birthDate;
 	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	public Integer getId() {
 		return id;
 	}
