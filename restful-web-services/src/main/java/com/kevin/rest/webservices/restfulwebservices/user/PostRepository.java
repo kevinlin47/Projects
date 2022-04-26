681dca800146c306199b8e2787058fd27e74fae1
Kevin Lin <kevinlin47@hotmail.com>
2022-04-26 19:48:10 -0400

    implementing a post service to create a post for a user

---
+++ restful-web-services/src/main/java/com/kevin/rest/webservices/restfulwebservices/user/PostRepository.java
@@ -0,0 +1,9 @@
+package com.kevin.rest.webservices.restfulwebservices.user;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer>{
+
+}
